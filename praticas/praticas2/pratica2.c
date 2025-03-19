8af594ce4cb412db1f15a6efcdfce3fb0f3ffb6d
JulioAdoraSopas <julioparafaculdade@gmail.com>
2025-03-19 10:54:08 -0300

    Adicionando arquivos da pratica02

---
+++ praticas/praticas2/pratica2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+
+printf("Julio Oliveira\n");
+
+printf("\nJulio Oliveira\n");
+
+printf("%i\n", 1234);
+
+printf("%5i\n", 123);
+printf("%5i\n", 56789);
+
+printf("%05i\n", 1);
+
+//coordenadas
+
+printf("(%i, %i)\n", -1, 1);
+
+//datas
+
+printf("%02i/%02i/%4i\n",19, 03, 2024);
+
+
+
+    return 0;
+}
