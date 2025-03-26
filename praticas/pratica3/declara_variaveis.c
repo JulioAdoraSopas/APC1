4f0f088cb0828e0665e3f94ddaab26f9456eeed5
JulioAdoraSopas <julioparafaculdade@gmail.com>
2025-03-26 10:06:06 -0300

    Adicionando arquivos da pratica03

---
+++ praticas/pratica3/declara_variaveis.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main () {
+
+int idade = 25;
+
+printf("idade: %i\n", idade);
+
+float altura = 1.75f;
+
+printf ("altura: %.2f\n", altura);
+
+char tecla = 'A';
+
+printf("Tecla: %c\n", tecla);
+
+double numero_e = 2.7182818284;
+
+printf("euler: %.10f\n", numero_e);
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ praticas/pratica3/tamanho_tipo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("O tipo 'char' ocupa %lu byte(s) na memória.\n", sizeof(char));
+
+    printf("o tipo 'int' ocupa %lu byte(s) na memoria.\n", sizeof (int));
+
+
+    printf("O tipo 'float' ocupa %lu byte(s) na memoria.\n", sizeof(float));
+
+    printf("O tipo 'double' ocupa %lu byte(s) na memoria. \n", sizeof(double));
+
+    printf("o tipo 'void' ocupa %lu byte(s) na memoria. \n", sizeof(void));
+
+    
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
