4025cbe7f6e9e65f0844000ae1f15e4668c691e0
JulioAdoraSopas <julioparafaculdade@gmail.com>
2025-05-28 10:39:48 -0300

    anotações do trabalho e outras coisas

---
+++ trabai/aula9.c
@@ -0,0 +1,68 @@
+// Cadastro.c : CADASTRO DE CLIENTES
+
+#include <stdio.h>
+
+int main () {
+
+    //entradas: int e char e long int
+// int nome;
+//´´ telefone;
+//´´ email;
+//´´ logradouro;
+//´´ numero;
+//´´ cep;
+//´´ complemento;
+// ´´ cidade;
+// ´´ uf.
+
+// -------------------------------------""----------------------------------------------
+
+// ESTRUTURA - STRUCT (PARA JUNTAR TODAS AS VARIAVEIS)
+//STRUCT.CLIENTE_EXEMPLO
+//CLIENTE.EXEMPLO
+
+//                                      MDC 118
+
+/* 
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    struct cliente_t
+    {
+        char nome[61];
+        int telefone;
+        char email[61];
+        char logradouro[61];
+        int numero;
+        int cep;
+        char complemento[61];
+        char cidade[41];
+        char uf[3];
+    };
+
+    struct cliente_t cliente;
+    strcpy(cliente.nome, "JOSE");
+    cliente.telefone = 61111111;
+    strcpy(cliente.email, "jose@iesb.br");
+    strcpy(cliente.logradouro, "sqs");
+    cliente.numero = 612;
+    cliente.cep = 70000000;
+    strcpy(cliente.complemento, "iesb");
+    strcpy(cliente.cidade, "Brasilia");
+    strcpy(cliente.uf, "DF");
+
+    printf("Dados do cliente\n");
+    printf("Nome: %s\n", cliente.nome);
+    printf("Telefone: %i\n", cliente.telefone);
+    printf("E-mail: %s\n", cliente.email);
+    printf("Endereco: %s, %i - %s - %s/%s\n", cliente.logradouro, cliente.numero, cliente.complemento, cliente.cidade, cliente.uf);
+    printf("CEP: %i", cliente.cep);
+    
+    return 0;
+}
+
+*/
+
+    return (0);
+}
